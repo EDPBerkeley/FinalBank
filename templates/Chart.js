ce4caf733037fbb362ea0dfb84eed596d4081056
Erchis Patwardhan <erchispatwardhan@berkeley.edu>
2021-01-04 10:21:10 -0500

    Fixed multiple graphs

--- templates/Chart.js
+++ templates/Chart.js
@@ -1,8 +1,99 @@
-let ctx = document.getElementById('myChart').getContext('2d');
+let ctx7 = document.getElementById("myChart").getContext('2d');
 let labels = ['Banking', 'Finances', 'Shopping', 'Recreation', 'Healthcare', 'Transportation', 'Food and Drink'];
 let colorHex = ['#FB3640', '#86A9C5', '#286CA1', '#77FAC', '#1C203D', '#798E9C', '#2A303D'];
 
-let myChart = new Chart(ctx, {
+let myChart = new Chart(ctx7, {
+  
+  type: 'pie',
+  data: {
+    datasets: [{
+      data: [10, 10, 10, 10, 10, 10, 10],
+      backgroundColor: colorHex,
+      borderWidth: [0,0,0,0,0,0,0]
+    }],
+    labels: labels,
+  
+  },
+  options: {
+    responsive: false,
+    legend: {
+      position: 'bottom'
+    },
+    
+    
+    plugins: {
+      
+      datalabels: {
+        color: '#fff',
+        anchor: 'end',
+        align: 'start',
+        offset: 10,
+        borderWidth: 0,
+        // borderColor: '#fff',
+        borderRadius: 0,
+        
+        font: {
+          size: '10',
+          color: '#fff'
+        },
+        formatter: (value) => {
+          return value + ' %';
+        }
+      }
+    }
+  }
+})
+
+
+let ctx2 = document.getElementById('myChart2').getContext('2d');
+let labels2 = ['Banking', 'Finances', 'Shopping', 'Recreation', 'Healthcare', 'Transportation', 'Food and Drink'];
+let colorHex2 = ['#FB3640', '#86A9C5', '#286CA1', '#77FAC', '#1C203D', '#798E9C', '#2A303D'];
+let myChart2 = new Chart(ctx2, {
+  
+  type: 'pie',
+  data: {
+    datasets: [{
+      data: [10, 10, 10, 10, 10, 10, 10],
+      backgroundColor: colorHex,
+      borderWidth: [0,0,0,0,0,0,0]
+    }],
+    labels: labels,
+  
+  },
+  options: {
+    responsive: false,
+    legend: {
+      position: 'bottom'
+    },
+    
+    
+    plugins: {
+      
+      datalabels: {
+        color: '#fff',
+        anchor: 'end',
+        align: 'start',
+        offset: 10,
+        borderWidth: 0,
+        // borderColor: '#fff',
+        borderRadius: 0,
+        
+        font: {
+          size: '10',
+          color: '#fff'
+        },
+        formatter: (value) => {
+          return value + ' %';
+        }
+      }
+    }
+  }
+})
+
+let ctx3 = document.getElementById('myChart3').getContext('2d');
+let labels3 = ['Banking', 'Finances', 'Shopping', 'Recreation', 'Healthcare', 'Transportation', 'Food and Drink'];
+let colorHex3 = ['#FB3640', '#86A9C5', '#286CA1', '#77FAC', '#1C203D', '#798E9C', '#2A303D'];
+let myChart3 = new Chart(ctx3, {
   
   type: 'pie',
   data: {
