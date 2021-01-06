910e1332562d99f7c908ed9d26469f3dbb58f342
Erchis Patwardhan <erchispatwardhan@berkeley.edu>
2021-01-06 17:41:10 -0500

    Potentially finished frontend

--- static/Chart.js
+++ static/Chart.js
@@ -2,8 +2,7 @@ let ctx1 = document.getElementById("myChart").getContext('2d');
 let labels = ['Banking', 'Finances', 'Shopping', 'Recreation', 'Healthcare', 'Transportation', 'Food and Drink'];
 let colorHex = ['#FB3640', '#86A9C5', '#286CA1', '#77FAC', '#1C203D', '#798E9C', '#2A303D'];
 var chart1Data = JSON.parse(chart1)
-let data1 = [chart1Data["Banking"],chart1Data["Finances"], chart1Data["Shopping"],
-  chart1Data["Recreation"], chart1Data["Food and Drink"], chart1Data["Transportation"]];
+let data1 = [chart1["Banking"],chart1["Finances"], chart1["Shopping"], chart1["Recreation"],chart1["Healthcare"],chart1["Transportation"], chart1["Food and Drink"]];
 console.log(chart1Data["Banking"])
 
 let myChart = new Chart(ctx1, {
@@ -55,12 +54,14 @@ let myChart = new Chart(ctx1, {
 let ctx2 = document.getElementById('myChart2').getContext('2d');
 let labels2 = ['Banking', 'Finances', 'Shopping', 'Recreation', 'Healthcare', 'Transportation', 'Food and Drink'];
 let colorHex2 = ['#FB3640', '#86A9C5', '#286CA1', '#77FAC', '#1C203D', '#798E9C', '#2A303D'];
+let data2 = [chart2["Banking"],chart2["Finances"], chart2["Shopping"], chart2["Recreation"],chart2["Healthcare"], chart2["Food and Drink"], chart2["Transportation"]];
+console.log(chart2["Banking"])
 let myChart2 = new Chart(ctx2, {
   
   type: 'pie',
   data: {
     datasets: [{
-      data: [15, 20, 17, 4, 8, 9, 20],
+      data: data2,
       backgroundColor: colorHex,
       borderWidth: [0,0,0,0,0,0,0]
     }],
@@ -151,13 +152,21 @@ let myChart2 = new Chart(ctx2, {
 
 let ctx4 = document.getElementById('myChart4').getContext('2d');
 let colorHex4 = ['#FB3640', '#86A9C5', '#286CA1', '#77FAC', '#1C203D', '#798E9C', '#2A303D'];
+
+//Loop through the chart4 array and add it to the data4 array
+var data4 = [];
+for(i=1; i<=31; i++) {
+  data4.push(chart4[i]);
+}
+
+console.log(chart2["Banking"])
 let myChart4 = new Chart(ctx4, {
   type: 'line',
   data: {
     labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
     ,
     datasets: [{ 
-        data: [74, 12, 63, 49, 91, 85, 56, 15, 17, 9, 47, 57, 97, 81, 64, 73, 89, 22, 19, 70, 22, 97, 66, 81, 59, 29, 74, 46, 34, 49, 39],
+        data: data4,
         borderColor: "#E61616",
         
         fill: true,
@@ -211,13 +220,18 @@ let myChart4 = new Chart(ctx4, {
 
 
 let ctx5 = document.getElementById('myChart5').getContext('2d');
+//Loop through the chart4 array and add it to the data4 array
+var data5 = [];
+for(i=1; i<=12; i++) {
+  data4.push(chart5[i]);
+}
 let myChart5 = new Chart(ctx5, {
   type: 'line',
   data: {
     labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
     ,
     datasets: [{ 
-        data: [74, 12, 63, 49, 91, 85, 56, 15, 17, 9, 47, 57],
+        data: data5,
         borderColor: "#E61616",
         
         fill: true,
